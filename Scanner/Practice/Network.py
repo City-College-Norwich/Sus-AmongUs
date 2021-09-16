48900f27a63b219498902a8ba0e4ff775327aaed
Richard Roth <90181549+Professor-Woody@users.noreply.github.com>
2021-09-16 13:43:33 +0100

    Adding practice files

---
+++ Scanner/Practice/Network.py
@@ -0,0 +1,15 @@
+""" Create a new class called Network 
+with the __init__ and __update__ functions """
+
+# Task 1: 
+# create the class here
+# your code here:
+# --------------------------------
+
+
+
+
+
+
+# --------------------------------
+
