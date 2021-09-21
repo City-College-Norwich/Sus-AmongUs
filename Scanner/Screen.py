a82c388576435b36666e8399c5cfbea12b9f3d9c
k3loades <k3loades@student.ccn.ac.uk>
2021-09-21 10:40:00 +0100

    Update Screen.py

--- Scanner/Screen.py
+++ Scanner/Screen.py
@@ -6,7 +6,7 @@ import ssd1306
 class Screen:
     def __init__(self):
         i2c = I2C(sda=Pin(0), scl=Pin(1)) #Fix Pins
-        self.display = ssd1306.SSD1306_I2C(126, 64, i2c)
+        self.display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
     def display_text(self, text, x_coordinate, y_coordinate):
         self.display.text(text, x_coordinate, y_coordinate)
