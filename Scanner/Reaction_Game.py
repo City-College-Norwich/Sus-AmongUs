adeb7d604c965c3ce2bbce11a9b61da3d4ab24ae
k3loades <k3loades@student.ccn.ac.uk>
2021-09-21 13:54:03 +0100

    Create Reaction_Game.py

---
+++ Scanner/Reaction_Game.py
@@ -0,0 +1,39 @@
+import random
+import time
+import Buttons
+import WiFi
+
+class Reaction_Game:
+    def __init__(self, parent):
+        Minigame.__init__(self, parent)
+        self.parent = parent
+
+    def update():
+        pass
+
+    def alertFromServer(self, alert):
+        if alert == 'GameStarted':
+            self.parent.currentMiniGame = Reaction_Game()
+
+    def reaction_game(self.parent.rfid):
+        self.can_press_button = False
+        time_to_change = Random.randint(5,16)
+        self.time_passed = time.ticks_ms()/1000
+
+        if self.time_passed == time_to_change:
+            display.fill(1)
+            self.can_press_button = True
+
+    def Reaction_Button_Pressed(self):
+        time_to_change = 2
+        self.time_passed = time.ticks_ms()/1000
+
+        if self.time_passed == time_to_change:
+            display.fill(0)
+            self.can_press_button = False
+            #Game starts over
+
+        elif self.can_press_button == True:
+            Buttons.getPressedButton(self)
+            if self.__button0.value() == 1:
+                WiFi.send_request(self, "Reaction_Game Complete")
