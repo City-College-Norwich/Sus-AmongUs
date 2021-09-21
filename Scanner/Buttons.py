f7cad1fa15cbbe7698e38f6bb868319da7f01fbc
GoodGuyKali <72386739+GoodGuyKali@users.noreply.github.com>
2021-09-21 10:25:15 +0100

    Update Buttons.py

--- Scanner/Buttons.py
+++ Scanner/Buttons.py
@@ -3,12 +3,12 @@ from machine import Pin
 
 class Buttons:
 
-    def __init__():
+    def __init__(self):
 
         self.__button0 = Pin(34, Pin.IN)
         self.__button1 = Pin(35, Pin.IN)
         self.__button2 = Pin(36, Pin.IN)
         self.__button3 = Pin(37, Pin.IN)
 
-    def getPressedButtons():
+    def getPressedButtons(self):
         return (self.__button0.value(), self.__button1.value(), self.__button2.value(), self.__button3.value())
