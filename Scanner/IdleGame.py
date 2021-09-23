476bef674b2becefe2488551ec00d8a1f8230fe7
Jacob Neave <35272149+AggrievedBubble@users.noreply.github.com>
2021-09-23 14:57:32 +0100

    Update IdleGame.py

--- Scanner/IdleGame.py
+++ Scanner/IdleGame.py
@@ -17,26 +17,19 @@ class IdleGame(Minigame):
         Minigame(self, parent)
         self.parent = parent
 
-        # update bellow tuple with minigames
-        self.__minigames = (IdBadge, Reaction_Game)
-        self.__target_station = "stationAssignment"
+        # update bellow set with minigames
+        self.__minigames = {IdBadge, Reaction_Game}
+        self.__target_station = self.parent.wifi.send_request("requestStation")
 
     def update(self):
         tag = self.parent.rfid.do_read()
 
-        if tag == self.__target_station and self.__target_station == "stationAssignment":
-
-            if self.__target_station == "stationAssignment":
-                self.__target_station = self.parent.wifi.send_request("requestStation")
-                self.parent.screen.clear_screen()
-                self.parent.screen.display_text("GOTO: " + self.__target_station)
-
-            else:
+        if tag == self.__target_station:
                 self.parent.currentMiniGame = random.choice(self.__minigames.__init__())
 
         else:
             self.parent.screen.clear_screen()
-            self.parent.screen.display_text("GOTO: " + self.__target_station)
+            self.parent.screen.display_text(f"GOTO:{' ':<3}{self.__target_station:>8}")
 
 
     def alertFromServer(self, alert):
