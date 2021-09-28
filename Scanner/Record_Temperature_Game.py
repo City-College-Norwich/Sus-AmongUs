763bdea775d9db8130c36caa182ba0074db5075c
k3loades <k3loades@student.ccn.ac.uk>
2021-09-28 11:53:26 +0100

    Update Record_Temperature_Game.py

--- Scanner/Record_Temperature_Game.py
+++ Scanner/Record_Temperature_Game.py
@@ -14,7 +14,7 @@ class Record_Temperature_Game:
     def update(self):
         self.parent.screen.display_text("Current Temperature: " + self.current_temperature, 0, 0)
         self.parent.screen.display_text("Logged Temperature: " + self.logged_temperature, 0, 20)
-        if self.logged_temperature = self.current_temperature:
+        if self.logged_temperature == self.current_temperature:
             self.parent.wifi.send_request(self, "minigameComplete?scannerId="+self.parent.id)
             self.parent.currentMiniGame = IdleGame()
         else:
