3ee2253954ee8d2c82373332a5c91804493c3197
Richard Roth <90181549+Professor-Woody@users.noreply.github.com>
2021-09-28 14:16:47 +0100

    fixing a conflict error

--- Scanner/Rfid.py
+++ Scanner/Rfid.py
@@ -3,16 +3,12 @@ from TimerHelper import TimerHelper
 class Rfid:
 
     def __init__(self):
-<<<<<<< Updated upstream
-        self.rdr = mfrc522.MFRC522(12, 2, 17, 0, 13)
+        self.rdr = mfrc522.MFRC522(26, 27, 14, 12, 25)
         self.timer = TimerHelper()
         self.timer.Set(100)
         self.timerSet = False
 
-=======
-        self.rdr = mfrc522.MFRC522(26, 27, 14, 12, 25)
-        
->>>>>>> Stashed changes
+
     def update(self):
         pass
 
