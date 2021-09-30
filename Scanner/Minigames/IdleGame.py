cb57d7c8bf9423b4cd1a6478a874b7e6ad962205
Conni <conni@spookiebois.club>
2021-09-30 15:10:48 +0100

    Fixed

--- Scanner/Minigames/IdleGame.py
+++ Scanner/Minigames/IdleGame.py
@@ -7,8 +7,8 @@ from Minigame import Minigame
  from {file} import {class}                   
  e.g. from WiresMinigame import WiresMinigame 
 '''
-from IdBadge import IdBadge
-from ReactionGame import ReactionGame
+from Minigames.IdBadge import IdBadge
+from Minigames.ReactionGame import ReactionGame
 
 
 class IdleGame(Minigame):
