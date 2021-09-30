e7f73a6d6fb50d573638520a5254d987a75b3818
k3loades <k3loades@student.ccn.ac.uk>
2021-09-30 15:51:54 +0100

    Update model.py

--- api/model.py
+++ api/model.py
@@ -14,25 +14,26 @@ class Model:
         self.players ={99:['player_uid', 'team'],}
         self.Crewmate = 0
         self.Imposter = 0
+        self.totalImposters = 2
 
     def getTagName(self, uid):
         return self.uids[uid] 
 
     def startGame(self):
         self.state = "Game_Running"
-        for i in range(0, len(self.players )):
+        for i in range(0, len(self.players)):
             team_assigner = random.randint(0,2)
-            if self.Crewmate == 8:
-                team_assigner == 1
+            if self.Crewmate == len(self.players) - self.totalImposters:
+                team_assigner = 1
 
-            if self.Imposter == 2:
-                team_assigner == 0
+            if self.Imposter == self.totalImposters:
+                team_assigner = 0
 
             if team_assigner == 0 :
-                self.players [i][1] = "Crewmate"
+                self.players[i][1] = "Crewmate"
                 self.Crewmate +=1
             elif team_assigner == 1:
-                self.players [i][1] = "Imposter"
+                self.players[i][1] = "Imposter"
                 self.Imposter += 1
         return self.state
 
