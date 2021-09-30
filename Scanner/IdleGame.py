84934b9029615f0a877cc48c3b13dae53e07aa7e
BigAl0161 <a1dunseith@student.ccn.ac.uk>
2021-09-30 09:58:44 +0100

    IdleGame_Ending
    If the game is no longer running, game ends. Displaying a message too!

--- Scanner/IdleGame.py
+++ Scanner/IdleGame.py
@@ -10,26 +10,41 @@ from Minigame import Minigame
 from IdBadge import IdBadge
 from ReactionGame import ReactionGame
 
-
+RUNNING = 0
+ENDED = 1
 class IdleGame(Minigame):
 
     def __init__(self, parent):
         Minigame(self, parent)
         self.parent = parent
-
+        # Add state variable 
+        self.state = RUNNING
+        
         # update bellow set with minigames
         self.__minigames = {IdBadge, ReactionGame}
         self.__target_station = self.parent.wifi.sendRequest("requestStation")
 
-    def update(self):
-        targetRfidTag = self.parent.rfid.do_read()
+    def update(self): 
+        # If state = running 
+            # then do all of that
+        if self.state == RUNNING
+           
+            targetRfidTag = self.parent.rfid.do_read()
 
-        if targetRfidTag == self.__target_station:
-            self.parent.currentMiniGame = random.choice(self.__minigames.__init__())
-        else:
-            self.parent.screen.clear_screen()
-            self.parent.screen.display_text("GOTO: " + str(self.__target_station))
-
+            if targetRfidTag == self.__target_station:
+                self.parent.currentMiniGame = random.choice(self.__minigames.__init__())
+            else:
+                self.parent.screen.clear_screen()
+                self.parent.screen.display_text("GOTO: " + str(self.__target_station))
+        #elseif gamestopped 
+        #display game ended message 
+        elif self.state == ENDED:
+            self.parent.screen.display_text("Game Over!")
 
     def alertFromServer(self, alert):
         pass
+        if alert == 'Gameended':
+            self.state = ENDED
+        # If alert = game ended
+            #Then set state to game ended
+        
