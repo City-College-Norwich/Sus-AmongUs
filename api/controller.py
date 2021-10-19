9aaa4f94287116c47941e19d30245071e833c2d8
e-tunez <e1antunes@student.ccn.ac.uk>
2021-10-19 11:58:51 +0100

    Bug fixes

--- api/controller.py
+++ api/controller.py
@@ -79,7 +79,7 @@ def sabotageCompleted():
 @app.route("/isAlive")
 def isAlive():
     args = request.args
-    return model.isAlive(args[badgeUID])
+    return model.isAlive(args['badgeUID'])
 
 
 if __name__ == '__main__': app.run(host='0.0.0.0')
