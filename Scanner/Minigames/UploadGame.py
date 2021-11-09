824091da8da7f4fb134a9937e356240c4127ca1a
Jacob Neave <35272149+aircooledjacob@users.noreply.github.com>
2021-11-09 13:51:00 +0000

    Update UploadGame.py

--- Scanner/Minigames/UploadGame.py
+++ Scanner/Minigames/UploadGame.py
@@ -17,7 +17,7 @@ class UploadGame(Minigame):
         if self.rfid:
             if self.timer.check():
                 self.progress_width += random.choice(range(5, 16))
-                self.progress = self.progress+10
+                self.progress = self.progress_width
                 self.parent.screen.drawText("Keep Scanning", 0, 0)
                 self.parent.screen.drawRectangle(10, 20, self.progress_width, 15)
                 self.parent.screen.drawText(str(self.progress) + "%", 50, 45)
