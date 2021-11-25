b73b97d20d0c0445423d903c2eb2ff6276dad19f
Richard Roth <90181549+Professor-Woody@users.noreply.github.com>
2021-11-25 15:40:17 +0000

    Fixed upload and downloadgame
    fixes #317

--- Scanner/Minigames/DownloadGame.py
+++ Scanner/Minigames/DownloadGame.py
@@ -12,29 +12,31 @@ class DownloadGame(Minigame):
         self.timer.set(1000)
         self.rfid = False
 
-        pass
 
     def update(self):
         self.rfid = self.parent.rfid.doRead()
-        self.parent.screen.clear()
-        self.parent.screen.drawText("Download Game", 0, 0)
+
         if self.rfid:
             if self.timer.check():
                 self.progress += random.choice(range(5, 16))
                 if self.progress > 100:
                     self.progress = 100
-                self.parent.screen.drawText("Keep Scanning", 0, 10)
-                self.parent.screen.drawRectangle(10, 20, self.progress, 25)
-                self.parent.screen.drawText(str(self.progress) + "%", 50, 55)
+                self.drawUI()
                 self.timer.set(1000)
 
         else:
-            self.parent.screen.drawText("Keep Scanning", 0, 10)
-            self.parent.screen.drawRectangle(10, 20, self.progress, 25)
-            self.parent.screen.drawText(str(self.progress) + "%", 50, 55)
+            self.drawUI()
+
 
         if self.progress >= 100:
             self.parent.wifi.completeMinigame(self.parent.badgeUID)
             self.parent.isMinigameCompleted = True
             self.parent.lastMinigame = DownloadGame
             self.parent.gotoIdleGame()
+
+    def drawUI(self):
+        self.parent.screen.clear()
+        self.parent.screen.drawText("Download Game", 0, 0)
+        self.parent.screen.drawText("Keep Scanning", 0, 10)
+        self.parent.screen.drawRectangle(10, 20, self.progress, 25)
+        self.parent.screen.drawText(str(self.progress) + "%", 50, 55)
