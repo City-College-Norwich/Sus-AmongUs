b0d0594830629c6e9d27f7d932e24583bdfb5e7b
e-tunez <e1antunes@student.ccn.ac.uk>
2021-11-25 10:53:07 +0000

    Update VotingGame.py

--- Scanner/Minigames/VotingGame.py
+++ Scanner/Minigames/VotingGame.py
@@ -13,11 +13,6 @@ class VotingGame(Minigame):
         self.parent.screen.drawText("Go to vote room ", 0, 20)
         self.parent.screen.drawText("      and", 0, 30)
         self.parent.screen.drawText(" Scan vote tag ", 0, 40)
-        '''
-        #Old display text - Not sure if is still needed
-        self.parent.screen.clear()
-        self.parent.screen.drawText("Voting Started", 0, 0)
-'''
 
         self.voted = False
         self.InitiateVoting = False
@@ -35,14 +30,14 @@ class VotingGame(Minigame):
                 self.parent.wifi.voteTimeEnd()
             if self.voted==False:
                 self.parent.screen.clear()
-                self.parent.screen.drawText("    Voting", 0, 0)
+                self.parent.screen.drawText("   -Voting-", 0, 0)
                 self.parent.screen.drawText("Scan player tag ", 0, 20)
                 self.parent.screen.drawText("    to vote ", 0, 30)
             if tag is not None and tag[:8] == 'playerId' and self.voted == False:
                 if self.parent.wifi.isAlive(self.parent.badgeUID):
                     self.parent.wifi.voteTally(uid, self.parent.badgeUID)
                 self.parent.screen.clear()
-                self.parent.screen.drawText("    Voting", 0, 0)
+                self.parent.screen.drawText("   -Voting-", 0, 0)
                 self.parent.screen.drawText(" You have voted. ", 0, 20)
                 self.voted = True
 
