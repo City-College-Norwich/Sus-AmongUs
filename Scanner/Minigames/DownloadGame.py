c1808d4a52b78d7e4fb7be5dc29378d7e98038c4
Richard Roth <90181549+Professor-Woody@users.noreply.github.com>
2021-11-25 15:33:32 +0000

    Fixing  (hopefully) an endless issue with downloadgame
    Also standardised the drawing

--- Scanner/Minigames/DownloadGame.py
+++ Scanner/Minigames/DownloadGame.py
@@ -8,7 +8,6 @@ class DownloadGame(Minigame):
         Minigame.__init__(self,parent)
         self.parent = parent
         self.progress = 0
-        self.progress_width = 0
         self.timer = TimerHelper()
         self.timer.set(1000)
         self.rfid = False
@@ -21,21 +20,21 @@ class DownloadGame(Minigame):
         self.parent.screen.drawText("Download Game", 0, 0)
         if self.rfid:
             if self.timer.check():
-                self.progress_width += random.choice(range(5, 16))
-                self.progress = self.progress_width
+                self.progress += random.choice(range(5, 16))
+                if self.progress > 100:
+                    self.progress = 100
                 self.parent.screen.drawText("Keep Scanning", 0, 10)
-                self.parent.screen.drawRectangle(10, 20, self.progress_width, 25)
+                self.parent.screen.drawRectangle(10, 20, self.progress, 25)
                 self.parent.screen.drawText(str(self.progress) + "%", 50, 55)
                 self.timer.set(1000)
 
-                if self.progress >= 100:
-                    self.parent.wifi.completeMinigame(self.parent.badgeUID)
-                    self.parent.isMinigameCompleted = True
-                    self.parent.lastMinigame = DownloadGame
-                    self.parent.gotoIdleGame()
-
-
         else:
-            self.parent.screen.drawText("Keep Scanning", 0, 0)
-            self.parent.screen.drawRectangle(10, 20, self.progress_width, 15)
-            self.parent.screen.drawText(str(self.progress) + "%", 50, 45)
+            self.parent.screen.drawText("Keep Scanning", 0, 10)
+            self.parent.screen.drawRectangle(10, 20, self.progress, 25)
+            self.parent.screen.drawText(str(self.progress) + "%", 50, 55)
+
+        if self.progress >= 100:
+            self.parent.wifi.completeMinigame(self.parent.badgeUID)
+            self.parent.isMinigameCompleted = True
+            self.parent.lastMinigame = DownloadGame
+            self.parent.gotoIdleGame()
