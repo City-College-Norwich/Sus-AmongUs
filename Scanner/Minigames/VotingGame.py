bed293b47110a86c39a94fa3b8afc3bcdf17f1a6
e-tunez <e1antunes@student.ccn.ac.uk>
2021-11-25 10:35:12 +0000

    changing text#1

--- Scanner/Minigames/VotingGame.py
+++ Scanner/Minigames/VotingGame.py
@@ -7,19 +7,17 @@ class VotingGame(Minigame):
 
         self.parent.screen.clear()
         if self.parent.votingType == 'meeting':
-            self.parent.screen.drawText("Emergency Meeting!", 0, 0)
+            self.parent.screen.drawText("--Emergency Meeting!--", 0, 0)
         elif self.parent.votingType == 'report':
-            self.parent.screen.drawText("Dead Body Reported!", 0, 0)
-        self.parent.screen.drawText("Go to Voting Room ", 0, 20)
-        
-        '''#Old display text - Not sure if is still needed
+            self.parent.screen.drawText("--Dead Body Reported!--", 0, 0)
+        self.parent.screen.drawText("Go to vote room ", 0, 20)
+        self.parent.screen.drawText("      and", 0, 30)
+        self.parent.screen.drawText(" Scan vote tag ", 0, 40)
+        '''
+        #Old display text - Not sure if is still needed
         self.parent.screen.clear()
         self.parent.screen.drawText("Voting Started", 0, 0)
-
-        self.parent.screen.drawText("Go to Voting", 0, 20)
-        self.parent.screen.drawText("Room", 0, 30)
-        self.parent.screen.drawText("Scan Vote tag ", 0, 50)
-
+'''
 
         self.voted = False
         self.InitiateVoting = False
@@ -35,12 +33,17 @@ class VotingGame(Minigame):
         else:
             if self.timer.check():
                 self.parent.wifi.voteTimeEnd()
-                
-            self.parent.screen.drawText("Voting", 0, 0)
-            self.parent.screen.drawText("Scan Player tag ", 0, 20)
+            if self.voted==False:
+                self.parent.screen.clear()
+                self.parent.screen.drawText("    Voting", 0, 0)
+                self.parent.screen.drawText("Scan player tag ", 0, 20)
+                self.parent.screen.drawText("    to vote ", 0, 30)
             if tag is not None and tag[:8] == 'playerId' and self.voted == False:
                 if self.parent.wifi.isAlive(self.parent.badgeUID):
                     self.parent.wifi.voteTally(uid, self.parent.badgeUID)
+                self.parent.screen.clear()
+                self.parent.screen.drawText("    Voting", 0, 0)
+                self.parent.screen.drawText(" You have voted. ", 0, 20)
                 self.voted = True
 
     def alertsFromServer(self, alerts):
