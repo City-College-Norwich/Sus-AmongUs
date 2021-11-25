71e42358e18132532e0960b08d9aeed37b9dfcc8
Jacob Neave <35272149+aircooledjacob@users.noreply.github.com>
2021-11-25 09:29:36 +0000

    fix timer

--- Scanner/Minigames/Sabotage2.py
+++ Scanner/Minigames/Sabotage2.py
@@ -11,6 +11,7 @@ class Sabotage2(Minigame):
         self.parent = parent
         self.__target_player = random.choice(self.parent.wifi.getPlayers())
         self.__delay_timer = TimerHelper()
+        self.__delay_timer.set(75000)
         # chooses a random player tag to be used for sabotage.
         if self.parent.badgeUID == self.__target_player:
             for x in self.parent.user_minigames_dict:
