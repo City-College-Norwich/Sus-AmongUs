031fd5c2b4ab88e4411a1eb7f7cf7897dafbfc93
k3loades <k3loades@student.ccn.ac.uk>
2021-12-07 09:14:32 +0000

    Update Wifi.py

--- Scanner/Wifi.py
+++ Scanner/Wifi.py
@@ -9,7 +9,7 @@ class Wifi:
     wlan = None
     SSID = "AmongstUsNet"
     PASSWORD = "AmongstUs"
-    URL = "http://192.1.1.1:5000/"
+    URL = "http://192.168.8.100/"
 
     def __init__(self, parent):
         self.parent = parent
